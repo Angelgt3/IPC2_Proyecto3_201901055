b79f162569c47182e49af874f1af2366721eaadc
Angelgt3 <angelaragon130@gmail.com>
2021-05-03 17:52:11 -0600

    entrada y salida del archivo xml

---
+++ Proyecto/app/Funciones.py
@@ -0,0 +1,143 @@
+import xml.etree.cElementTree as ET
+
+def separacion(ruta):
+    eventos=list()
+    estoy=False
+    lineas=list()
+    archivo = open(ruta, encoding="utf8")
+    for linear in archivo:
+        lineal=linear.rstrip()
+        linea=lineal.lstrip()
+        if linea=="<EVENTO>":
+        	estoy=True
+        elif linea=="</EVENTO>":
+            estoy=False
+            temp=[]
+            for i in lineas:
+                temp.append(i)
+            eventos.append(temp)
+            for l in range(len(lineas)):
+                lineas.pop()
+			#print(f"vector lineas {lineas}")
+			#print(f"vector eventos {eventos}")
+        elif estoy==True:
+        	lineas.append(linea)
+
+    Datos_Dia=list()
+    Datos=list()
+
+    for e in eventos:
+        contador=0
+        for l in e:
+            if contador==0:
+                fechar=l.split(",")
+                fechal=fechar[1].rstrip()
+                fecha=fechal.lstrip()
+                #print(f"fecha: {fecha}")
+                Datos_Dia.append(fecha)
+                contador+=1
+            elif contador==1:
+                repot1=l.split('"')
+                repot2=repot1[2].split(">")
+                reportado=repot2[0].lstrip()
+                #print(f"Reportado: {reportado}")
+                Datos_Dia.append(reportado)
+                contador+=1
+            elif contador==2:
+                l2=l.replace("<","")
+                l3=l2.replace(">","")
+                l4=l3.split(":")
+                l5=l4[1].split(",")
+                Afectados=[]
+                for i in l5:
+                    #print(f"Afectados: {i}")
+                    Afectados.append(i)
+                Datos_Dia.append(Afectados)
+                contador+=1
+            elif contador==3:
+                l2=l.split(":")
+                l3=l2[1].split("-")
+                codigo=l3[0]
+                #print(f"Codigo: {codigo}")
+                Datos_Dia.append(codigo)
+                contador+=1
+                temp=[]
+                for i in Datos_Dia:
+                    temp.append(i)
+                Datos.append(temp)
+                for l in range(len(Datos_Dia)):
+                    Datos_Dia.pop()
+    
+    
+    estadisticas =ET.Element("ESTADISTICAS")
+    estadisticas.text="\n\t"
+
+    while True:
+        estadistica = ET.SubElement(estadisticas, "ESTADISTICA")
+        estadistica.text="\n\t\t"
+        
+        posiciones=list()
+        ya=list()
+        for t in range(len(Datos)):
+            if Datos[0][0]==Datos[t][0]:
+                    posiciones.append(t)
+        
+
+        valor=Datos[posiciones[0]][0]
+        Fecha = ET.SubElement(estadistica,"FECHA")
+        Fecha.text=valor
+        Fecha.tail="\n\t\t"
+        Sumatoria = ET.SubElement(estadistica,"CANTIDAD_MENSAJES")
+        Sumatoria.text=str(len(posiciones))
+        Sumatoria.tail="\n\t\t"
+        Reportes = ET.SubElement(estadistica,"REPORTADO_POR")
+        Reportes.text="\n\t\t\t"
+        Reportes.tail="\n\t"
+        
+        te=list()
+        for b in posiciones:
+            te.append(Datos[b][1])
+        while True:
+            correo=te[0]
+            
+            cantidad=te.count(correo)
+            Usuario= ET.SubElement(Reportes,"USUARIO")
+            Usuario.text="\n\t\t\t\t"
+            
+            Email= ET.SubElement(Usuario,"EMAIL")
+            Email.text=correo
+            Email.tail="\n\t\t\t\t"
+            C_Mensaje=ET.SubElement(Usuario,"CANTIDAD_MENSAJES")
+            C_Mensaje.text=str(cantidad)
+            C_Mensaje.tail="\n\t\t\t"
+
+            co=0
+            for r in range(len(te)):
+                if te[co]==correo:
+                    te.pop(co)
+                    co-=1
+                co+=1
+            if len(te)==0:
+                Usuario.tail="\n\t\t"
+                break
+            else:
+                Usuario.tail="\n\t\t\t"
+
+        tamaño=len(Datos)
+        cont=0
+        for r in range(tamaño):
+            if Datos[cont][0]==valor:
+                Datos.pop(cont)
+                cont-=1
+            cont+=1
+        
+        if len(Datos)==0:
+            estadistica.tail="\n"
+            break
+        else:
+            estadistica.tail="\n\t"
+
+
+
+    Arbol=ET.ElementTree(estadisticas)
+    Arbol.write("estadistica.xml")
--- Proyecto/app/views.py
+++ Proyecto/app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from xml.dom import minidom
+from . import Funciones as Fun
 
 # Create your views here.
+def Login(request):
+    return render(request,'login.html')
+
+def Principal(request):
+    direA=request.GET["Direccion_AE"]
+    archivo = open(direA,encoding="utf8")
+    mensaje = archivo.read()
+    archivo.close()
+    Fun.separacion(str(direA))
+    return render(request,'principal.html',{"texto1":mensaje})
+
+
